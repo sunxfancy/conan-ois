b8a068fc810cac3bedfe216dfb12cd0d2637104d
Sixten Hilborn <hilborn90@gmail.com>
2017-01-20 23:52:53 +0100

    Disable Linux 32 bit in CI

--- build.py
+++ build.py
@@ -4,5 +4,11 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="OIS:shared", pure_c=False)
+    # Disable Linux 32 bit for now
+    builder.builds = [
+        [settings, options]
+        for settings, options in builder.builds
+        if not (settings.os == "Linux" and settings.arch == "x86")
+    ]
     builder.run()
 
