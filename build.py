8173ca1b9352fa2c905a4ed6ecd77a92491566b0
Sixten Hilborn <hilborn90@gmail.com>
2017-03-05 16:27:26 +0100

    Disable mac builds for now

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 from conan.packager import ConanMultiPackager
+from conans.tools import os_info
 import platform
 
 if __name__ == "__main__":
@@ -9,6 +10,7 @@ if __name__ == "__main__":
         [settings, options]
         for settings, options in builder.builds
         if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
+        and not (os_info.is_macos)
     ]
     builder.run()
 
