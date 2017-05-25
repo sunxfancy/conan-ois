db9355f57871cf2b69e6bbeec1db3972d3175274
Sixten Hilborn <hilborn90@gmail.com>
2017-05-25 17:22:38 +0200

    Fix build.py

--- build.py
+++ build.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     builder.add_common_builds(shared_option_name="OIS:shared", pure_c=False)
     # Disable VS2010 because of missing DirectX stuff
     builder.builds = [
-        [settings, options]
-        for settings, options in builder.builds
+        [settings, options, env_vars, build_requires]
+        for settings, options, env_vars, build_requires in builder.builds
         if not (settings["compiler"] == "Visual Studio" and settings["compiler.version"] == "10")
     ]
     builder.run()
